25811479c264d0f0d7790b044883f382fa44c133
CHIZI-0618 <2916997591@qq.com>
2021-07-14 22:04:19 +0800

    Simplify crontabs

--- service.sh
+++ service.sh
@@ -8,13 +8,12 @@ scripts_dir="${module_dir}/scripts"
 Clash_data_dir="/data/clash"
 Clash_run_path="${Clash_data_dir}/run"
 Clash_pid_file="${Clash_run_path}/clash.pid"
-busybox_path="/data/adb/magisk/busybox"
 
 if [ -f ${Clash_pid_file} ] ; then
-    rm -rf ${Clash_pid_file}
+    rm -f ${Clash_pid_file}
 fi
 
-nohup ${busybox_path} crond -c ${Clash_run_path} > /dev/null 2>&1 &
+crond -c ${Clash_run_path} > /dev/null 2>&1 &
 
 ${scripts_dir}/clash.service -s && ${scripts_dir}/clash.tproxy -s
 inotifyd ${scripts_dir}/clash.inotify ${module_dir} >> /dev/null &
