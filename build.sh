ae9d7b837990ee50f4046053c3f045553b2b565c
CHIZI-0618 <2916997591@qq.com>
2021-07-15 13:47:14 +0800

    Add module.prop file update

--- build.sh
+++ build.sh
@@ -12,4 +12,11 @@ cd ..
 
 cp -f ./clash/bin/clash-android-arm64 ./system/bin/clash
 
+echo "id=Clash_For_Magisk" > ./module.prop
+echo "name=Clash For Magisk" >> ./module.prop
+echo "version=${VERSION}" >> ./module.prop
+echo "versionCode=$(date +%Y%m%d)" >> ./module.prop
+echo "author=kalasutra" >> ./module.prop
+echo "description=利用tproxy支持Clash的透明代理.Clash内核为https://github.com/ClashDotNetFramework/experimental-clash版本以期支持providers.默认禁用ipv6." >> ./module.prop
+
 zip -r -o -X -ll -q Clash_For_Magisk_${VERSION}.zip system clash.config customize.sh META-INF scripts template module.prop service.sh uninstall.sh README.md yacd-gh-pages
